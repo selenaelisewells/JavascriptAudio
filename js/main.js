2cd7fafb607856518bf0d45f05ef0e06233f9454
Selena Wells <selena.elise.wells@gmail.com>
2020-06-19 12:02:19 -0400

    finished audio example

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 (() => {
     console.log('fired!');
 
-    let audioControls = document.querySelectorAll(".controls"),
-        audio = document.querySelector("audio");
+    let audioControls = document.querySelectorAll('.controls'),
+        audio = document.querySelector('audio'),
+        audioThumbs = document.querySelectorAll('.trackRef');
+
+    function loadAndPlay() {
+        //'this' keyword refers tot he image you've clicked on
+        audio.src = `audio/${this.dataset.trackref}`;
+        //load method loads whatever resource you indicate
+        audio.load();
+
+        playAudio();
+    }
 
     function playAudio() {
         audio.play();
@@ -14,6 +24,14 @@
 
     }
 
-    audioControls[0].addEventListener("click", playAudio);
-    audioControls[1].addEventListener("click", rewindAudio);
-})()
+    audioControls[0].addEventListener('click', playAudio);
+    audioControls[1].addEventListener('click', rewindAudio);
+    //process the image tags and make them load the right audio track
+
+    for (thumb of audioThumbs) {
+        thumb.addEventListener('click', loadAndPlay);
+    }
+
+    //listen for the end of the audio track
+    audio.addEventListener('ended', () => console.log('track ended'));
+})();
